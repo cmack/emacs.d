0d08f3a5c9cf174098d6a6bab9e61da584a11c05
Charlie McMackin <charliemac@gmail.com>
2012-09-27 15:19:34 -0500

    Hide various IRC messages / notifications in erc

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,8 +1,9 @@
-(setq erc-autojoin-channels-alist '(("irc.freenode.net" "#lisp" "#yui" "#sbcl" "#ruby"))
+(setq erc-autojoin-channels-alist '(("#lisp" "#yui" "#sbcl" "#ruby"))
       erc-email-userid "charlie"
       erc-modules '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate)
       erc-port 6667
       erc-server "irc.freenode.net"
-      erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477"))
+      erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477")
+      erc-hide-list '("JOIN" "NICK" "PART" "QUIT")))
 
 (provide 'init-erc)
