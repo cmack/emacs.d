77c5921e02f9748865ae27d998b4d818ccbd3cd6
Charlie McMackin <charliemac@gmail.com>
2012-10-25 10:59:50 -0500

    Slightly better handling of yasnippets in auto-complete-mode

--- init/init-auto-complete.el
+++ init/init-auto-complete.el
@@ -19,7 +19,9 @@
 
 
 (set-default 'ac-sources
-             '(ac-source-dictionary
+             '(ac-source-yasnippet
+               ac-source-abbrev
+               ac-source-dictionary
                ac-source-words-in-buffer
                ac-source-words-in-same-mode-buffers
                ac-source-words-in-all-buffer))
--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -6,4 +6,7 @@
              (concat (file-name-directory (locate-library "yasnippet"))
                      "snippets"))
 
+(add-hook 'html-mode-hook (lambda ()
+                            (setq ac-sources '(ac-source-semantic ac-source-yasnippet))))
+
 (provide 'init-yasnippet)
