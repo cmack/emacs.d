58cc28c8484b81b32784a083da8edbbc31434b52
Charlie McMackin <charliemac@gmail.com>
2013-03-29 15:38:42 -0500

    switch to Adobe Source Code Pro as first choice font

--- init/init-appearence.el
+++ init/init-appearence.el
@@ -2,7 +2,8 @@
 
 ;; TODO see if there's a better way
 (when *linux-p*  
-  (add-to-list 'default-frame-alist '(font . "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" )))
+  ;; (add-to-list 'default-frame-alist '(font . "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" ))
+  (add-to-list 'default-frame-alist '(font . "-adobe-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" )))
 
 (when *mac-p*  
   (add-to-list 'default-frame-alist '(font . "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" )))
