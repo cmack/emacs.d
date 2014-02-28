7d28a65d3eb81bc661c65ff3c0e0ead7eb7b799d
Charlie McMackin <charliemac@gmail.com>
2014-02-28 06:01:58 -0600

    Add support for coffee-mode
    * includes hacky way of getting tabs instead of spaces... I had no
      choice, work standards :(

--- init/init-javascript.el
+++ init/init-javascript.el
@@ -6,4 +6,24 @@
                            (setq js2-consistent-level-indent-inner-bracket-p t)
                            (setq js2-pretty-multiline-decl-indentation-p t)))
 
+(defun customize-coffee ()
+  "my custom coffee-mode hook"
+  ;; (setq (make-local-variable 'tab-width) 2)
+  ;; (setq (make-local-variable 'indent-tabs-mode) t)
+  (setq tab-width 2)
+  (setq coffee-tab-width 2)
+  (setq indent-tabs-mode t)
+  (setq show-trailing-whitespace t)
+  (add-hook 'before-save-hook (lambda ()
+                                (delete-trailing-whitespace)
+                                ;; This is a sad hack. coffee-mode indents strictly
+                                ;; by spaces and I don't have time to patch.
+                                ;; I need tabs for work standards.
+                                (tabify-buffer))
+            nil t))
+
+(autoload 'coffee-mode "coffee-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.em$" . coffee-mode))
+(add-hook 'coffee-mode-hook 'customize-coffee)
+
 (provide 'init-javascript)
