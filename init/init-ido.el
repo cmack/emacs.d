09d0c72a6deca461ebffb193c9232b63d8613cb3
Charlie McMackin <charliemac@gmail.com>
2014-05-09 06:06:54 -0500

    ido customizations plus vertical menu
    mostly cribbed from purcell iirc

--- init/init-ido.el
+++ init/init-ido.el
@@ -1,9 +1,28 @@
+;; Use C-f during file selection to switch to regular find-file
+(require 'ido)
+(require 'ido-vertical-mode)
+
 (ido-mode t)
 (ido-everywhere t)
 (setq ido-enable-flex-matching t
+      ido-use-filename-at-point nil
+      ido-auto-merge-work-directories-length 0
       ido-use-virtual-buffers t)
+(ido-vertical-mode 1)
 
-(smex-initialize)
-(global-set-key "\M-x" 'smex)
+(when (eval-when-compile (>= emacs-major-version 24))
+  (require-package 'ido-ubiquitous)
+  (ido-ubiquitous-mode t))
+
+;; Use smex to handle M-x
+(when (eval-when-compile (>= emacs-major-version 24))
+  (require-package 'smex)
+  (global-set-key [remap execute-extended-command] 'smex))
+
+(require-package 'idomenu)
+
+;; Allow the same buffer to be open in different frames
+(setq ido-default-buffer-method 'selected-window)
 
 (provide 'init-ido)
+
