28fe30fe437962bddc13011ada7cf51f40319da1
Charlie McMackin <charliemac@gmail.com>
2014-09-07 23:00:44 -0500

    Cleanup and improve php-mode a bit
    * remove my attempt at a custom C style for php and use the psr2 style
    
    * add php-extras package
    
    * enable projectile for php projects
    
    * modify projectile's ctag refresh command otherwise in Laravel
      projects, we generate 50MB+ TAGS files

--- init/init-php.el
+++ init/init-php.el
@@ -1,3 +1,4 @@
+(require-package 'php-extras)
 ;; (require 'mmm-auto)
 ;; (setq mmm-global-mode 'maybe)
 ;; (mmm-add-group 'fancy-html
@@ -20,21 +21,12 @@
 
 (add-hook 'php-mode-hook 'my-laravel-coding-style 'append)
 
-(c-add-style
- "laravel"
- '((c-basic-offset . 4)
-   (c-offsets-alist . ((arglist-intro . '+)
-                       (arglist-close . 0)
-                       (inline-open . 0)
-                       (substatement-open . 0)
-                       (block-open . 0)))))
-
 (defun my-laravel-coding-style ()
   "Makes php-mode use coding styles that are preferable for working
 with Laravel."
   (interactive)
-  ;; (flymake-mode 1)
-  (php-enable-pear-coding-style)
+  (flycheck-mode 1)
+  (php-enable-psr2-coding-style)
   (setq tab-width 4
         fill-column 119
         show-trailing-whitespace t
@@ -42,8 +34,18 @@ with Laravel."
   (turn-on-auto-fill)
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
   (c-set-offset 'case-label 4)
-  ;; (c-set-style "laravel")
   (subword-mode 1)
+  (projectile-mode 1)
+  (setq projectile-tags-command
+        (s-join " "
+                (list "ctags -Re"
+                      "--languages=-javascript,sql"
+                      "--exclude=.git"
+                      "--PHP-kinds=+cf"
+                      (shell-quote-argument "--regex-PHP='/abstract\s+class\s+([^ ]*)/\\1/c/'")
+                      (shell-quote-argument "--regex-PHP='/interface\s+([^ ]*)/\\1/c/'")
+                      (shell-quote-argument "--regex-PHP='/(public\s+|static\s+|abstract\s+|protected\s+|private\s+)+function\s+([^ (]*)/\2/f/'"))
+                ))
   )
 
 (provide 'init-php)
