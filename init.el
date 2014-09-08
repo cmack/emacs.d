358f33ed9428fad45f2b1711c333cddee338858a
Charlie McMackin <charliemac@gmail.com>
2014-09-07 22:50:23 -0500

    Turn off horizontal scroll-bar
    * in emacs 24.4

--- init.el
+++ init.el
@@ -12,6 +12,8 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
+
 (setq inhibit-startup-message t)
 
 (require 'init-behavior)
