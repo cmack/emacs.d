6c0ace45f6c547d28c7eddcd0559926b3872b03f
Charlie McMackin <charliemac@gmail.com>
2014-10-13 15:21:48 -0500

    Modify web-mode  autocomplete sources and css tabwidth

--- init/init-web.el
+++ init/init-web.el
@@ -20,10 +20,27 @@
             (setq indent-tabs-mode t
                   tab-width 4
                   web-mode-code-indent-offset 4
+                  web-mode-css-indent-offset 2
                   web-mode-markup-indent-offset 4)
+
+            (setq web-mode-ac-sources-list
+                  '(("php" . (ac-source-yasnippet))
+                    ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+                    ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
             (add-hook 'before-save-hook 'delete-trailing-whitespace)
             (subword-mode 1)
             (emmet-mode 1)))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+                   (yas-activate-extra-mode 'php-mode)
+                 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+                 (setq emmet-use-css-transform nil)))))
 
 (add-hook 'html-mode-hook
           (lambda ()
