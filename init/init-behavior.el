a7c7e1297caa8c577b4e02934c116da6e00652dd
Charlie McMackin <charliemac@gmail.com>
2015-06-27 14:13:28 -0500

    Fix typo

--- init/init-behavior.el
+++ init/init-behavior.el
@@ -41,7 +41,7 @@
 (set-terminal-coding-system 'utf-8)
 (prefer-coding-system       'utf-8)
 (set-default-coding-systems 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8)
 
 
 (when (>= emacs-major-version 24)
