6b2c871c36b483e9a0a077a796d73166fb0db1d0
Charlie McMackin <charliemac@gmail.com>
2015-07-25 09:09:38 -0500

    Convert init-web to use-package

--- init/init-web.el
+++ init/init-web.el
@@ -1,54 +1,51 @@
-;; (require 'multi-web-mode)
+(use-package haml-mode :ensure t :defer t)
+(use-package scss-mode :ensure t :defer t)
+(use-package sass-mode :ensure t :defer t)
+(use-package markdown-mode :ensure t :defer t)
 
-;; (setq mweb-default-major-mode 'html-mode)
-;; (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-;;                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-;;                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-;; (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-;; (multi-web-global-mode 1)
+(use-package web-mode
+  :ensure t
+  :defer t
+  :mode ("\\.hbs\\'"
+         "\\.jsx\\'"
+         "/\\([Vv]iews\\|[Hh]tml\\|[Tt]emplates\\)/.*\\.php\\'")
+  :config
+  (progn
+    (use-package emmet-mode :ensure t)
+    (setq sgml-basic-offset 4)
+    (setq indent-tabs-mode t
+          tab-width 4
+          web-mode-code-indent-offset 2
+          web-mode-css-indent-offset 2
+          web-mode-markup-indent-offset 4)
 
-(require 'web-mode)
-(require 'emmet-mode)
+    (setq web-mode-ac-sources-list
+          '(("php" . (ac-source-yasnippet))
+            ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+            ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+    (subword-mode 1)
+    (emmet-mode 1)
 
+    (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(add-to-list 'auto-mode-alist '("/\\([Vv]iews\\|[Hh]tml\\|[Tt]emplates\\)/.*\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+    (add-hook 'web-mode-before-auto-complete-hooks
+              '(lambda ()
+                 (let ((web-mode-cur-language (web-mode-language-at-pos)))
+                   (if (string= web-mode-cur-language "php")
+                       (yas-activate-extra-mode 'php-mode)
+                     (yas-deactivate-extra-mode 'php-mode))
+                   (if (string= web-mode-cur-language "css")
+                       (setq emmet-use-css-transform t)
+                     (setq emmet-use-css-transform nil)))))
 
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq sgml-basic-offset 4)
-            (setq indent-tabs-mode t
-                  tab-width 4
-                  web-mode-code-indent-offset 4
-                  web-mode-css-indent-offset 2
-                  web-mode-markup-indent-offset 4)
+    (add-hook 'html-mode 'web-mode)))
 
-            (setq web-mode-ac-sources-list
-                  '(("php" . (ac-source-yasnippet))
-                    ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
-                    ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
-
-            (add-hook 'before-save-hook 'delete-trailing-whitespace)
-            (subword-mode 1)
-            (emmet-mode 1)))
-
-(add-hook 'web-mode-before-auto-complete-hooks
-          '(lambda ()
-             (let ((web-mode-cur-language (web-mode-language-at-pos)))
-               (if (string= web-mode-cur-language "php")
-                   (yas-activate-extra-mode 'php-mode)
-                 (yas-deactivate-extra-mode 'php-mode))
-               (if (string= web-mode-cur-language "css")
-                   (setq emmet-use-css-transform t)
-                 (setq emmet-use-css-transform nil)))))
-
-(add-hook 'html-mode-hook
-          (lambda ()
-            (setq sgml-basic-offset 4)
-            (setq indent-tabs-mode t
-                  tab-width 4)
-            (add-hook 'before-save-hook 'delete-trailing-whitespace)
-            (subword-mode 1)
-            (emmet-mode 1)))
+(use-package sgml-mode
+  :defer t
+  :config
+  (progn
+    (use-package tagedit :ensure t)
+    (tagedit-add-paredit-like-keybindings)
+    (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1)))))
 
 (provide 'init-web)
