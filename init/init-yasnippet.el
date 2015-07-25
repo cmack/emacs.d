c4da488f16f6c596040aea706a10ccbe5a46d0e6
Charlie McMackin <charliemac@gmail.com>
2015-07-25 11:03:16 -0500

    Convert init-yasnippet to use-package

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -1,12 +1,16 @@
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas-global-mode 1)
+(use-package yasnippet
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (yas-global-mode 1)
 
-(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-(add-to-list 'yas-snippet-dirs
-             (concat (file-name-directory (locate-library "yasnippet"))
-                     "snippets"))
+    (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+    (add-to-list 'yas-snippet-dirs
+                 (concat (file-name-directory (locate-library "yasnippet"))
+                         "snippets"))
 
-(add-hook 'html-mode-hook (lambda ()
-                            (setq ac-sources '(ac-source-semantic ac-source-yasnippet))))
+    (add-hook 'html-mode-hook (lambda ()
+                                (setq ac-sources '(ac-source-semantic ac-source-yasnippet))))))
 
 (provide 'init-yasnippet)
