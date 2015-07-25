79e0457c89ce7ea2cc7769dd503a9ee96f6d3bc7
Charlie McMackin <charliemac@gmail.com>
2015-07-25 09:26:22 -0500

    Convert init-perl to use-package

--- init/init-perl.el
+++ init/init-perl.el
@@ -1,31 +1,36 @@
-(add-hook 'cperl-mode-hook (lambda ()
-                             (setq cperl-brace-offset 0)
+(use-package cperl-mode
+  :defer t
+  :ensure t
+  :config
+  (progn
+     (setq cperl-brace-offset 0)
 
-                             (setq cperl-indent-level 2
-                                   cperl-close-paren-offset (- cperl-indent-level))
+     (setq cperl-indent-level 2
+           cperl-close-paren-offset (- cperl-indent-level))
 
-                             (setq cperl-continued-brace-offset 0)
-                             (setq cperl-continued-statement-offset 0)
+     (setq cperl-continued-brace-offset 0)
+     (setq cperl-continued-statement-offset 0)
 
-                             (setq cperl-electric-keywords t
-                                   cperl-electric-parens t
-                                   cperl-electric-linefeed t
-                                   cperl-electric-lbrace-space t)
+     (setq cperl-electric-keywords t
+           cperl-electric-parens t
+           cperl-electric-linefeed t
+           cperl-electric-lbrace-space t)
 
-                             (when (>= emacs-major-version 24)
-                               (setq cperl-electric-parens nil
-                                     cperl-electric-lbrace-space nil))
+     (when (>= emacs-major-version 24)
+       (setq cperl-electric-parens nil
+             cperl-electric-lbrace-space nil))
 
-                             (setq cperl-merge-trailing-else nil)
-                             (setq cperl-brace-imaginary-offset 0)
-                             (setq cperl-indent-parens-as-block t)
-                             (setq cperl-indent-wrt-brace t)
-                             (setq cperl-label-offset -2)
-                             (setq cperl-merge-trailing-else t)
-                             (setq indent-tabs-mode t)
-                             (setq tab-width 2)
-                             (add-hook 'before-save-hook 'delete-trailing-whitespace)
-                             ))
+     (setq cperl-merge-trailing-else nil)
+     (setq cperl-brace-imaginary-offset 0)
+     (setq cperl-indent-parens-as-block t)
+     (setq cperl-indent-wrt-brace t)
+     (setq cperl-label-offset -2)
+     (setq cperl-merge-trailing-else t)
+     (setq indent-tabs-mode t)
+     (setq tab-width 2)
+     (add-hook 'before-save-hook 'delete-trailing-whitespace)
+     ))
+
 (mapc
      (lambda (pair)
        (if (eq (cdr pair) 'perl-mode)
