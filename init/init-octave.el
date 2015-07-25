96740508f55823446e10524e2b511c2e42590ace
Charlie McMackin <charliemac@gmail.com>
2015-07-25 18:58:02 -0500

    Convert init-octave to use-package

--- init/init-octave.el
+++ init/init-octave.el
@@ -1,8 +1,10 @@
-(autoload 'octave-mode "octave-mod" nil t)
-(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
-(add-hook 'octave-mode-hook (lambda ()
-                              (setq inferior-octave-program "~/bin/octave")
-                              (abbrev-mode 1)
-                              (auto-fill-mode 1)
-                              (if (eq window-system 'x) (font-lock-mode 1))
-                              ))
+(use-package octave-mode
+  :defer t
+  :ensure t
+  :mode ("\\.m$")
+  :config
+  (progn (setq inferior-octave-program "~/bin/octave")
+         (abbrev-mode 1)
+         (auto-fill-mode 1)
+         (if (eq window-system 'x) (font-lock-mode 1))
+         ))
