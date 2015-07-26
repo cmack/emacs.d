12edc465f71d6f6070ec5230af7cf1c59a7ce690
Charlie McMackin <charliemac@gmail.com>
2015-07-25 19:02:03 -0500

    Convert init-flycheck to use-package

--- init/init-flycheck.el
+++ init/init-flycheck.el
@@ -1,5 +1,6 @@
-(when (eval-when-compile (>= emacs-major-version 24))
-  (require-package 'flycheck)
+(use-package flycheck
+  :ensure t
+  :config
   (add-hook 'after-init-hook 'global-flycheck-mode))
 
 (provide 'init-flycheck)
