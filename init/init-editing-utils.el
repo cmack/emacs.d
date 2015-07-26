f28e5cc0248c8b3b5dbf77f618f25cf5e4d486fb
Charlie McMackin <charliemac@gmail.com>
2015-07-25 19:06:19 -0500

    Convert init-editing-utils to use-package

--- init/init-editing-utils.el
+++ init/init-editing-utils.el
@@ -1,13 +1,13 @@
-(require-package 'multiple-cursors)
-;; multiple-cursors
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-+") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-;; From active region to multiple cursors:
-(global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
-(global-set-key (kbd "C-c c c") 'mc/edit-lines)
-(global-set-key (kbd "C-c c e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-c c a") 'mc/edit-beginnings-of-lines)
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-<" . mc/mark-previous-like-this)
+         ("C->" . mc/mark-next-like-this)
+         ("C-+" . mc/mark-next-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)
+         ;; From active region to multiple cursors:
+         ("C-c c r" . set-rectangular-region-anchor)
+         ("C-c c c" . mc/edit-lines)
+         ("C-c c e" . mc/edit-ends-of-lines)
+         ("C-c c a" . mc/edit-beginnings-of-lines)))
 
 (provide 'init-editing-utils)
