3f195d055171bf2ae0253cf0c657268dc478cf42
Charlie McMackin <charliemac@gmail.com>
2015-08-04 21:20:09 -0500

    Defer yasnippet until YAS-GLOBAL-MODE eval

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -1,10 +1,10 @@
 (use-package yasnippet
   :ensure t
   :defer t
+  :commands yas-global-mode
   :config
   (progn
     (yas-global-mode 1)
-
     (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
     (add-to-list 'yas-snippet-dirs
                  (concat (file-name-directory (locate-library "yasnippet"))
