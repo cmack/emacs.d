784daff7afb4456fb8f51b439f0abd475749640e
Charlie McMackin <charlie@appa.attlocal.net>
2015-11-02 08:26:22 -0600

    Add better deferrment to erc config
    * Use :commands to defer loading
    * remove PROGN from :config

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,15 +1,14 @@
 (use-package erc
-  :defer t
+  :commands erc
   :ensure t
   :config
-  (progn
-    (setq erc-nick "cmack"
-          erc-autojoin-channels-alist '(("#lisp" "#yui" "#sbcl" "#ruby"))
-          erc-email-userid "charlie"
-          erc-modules '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate)
-          erc-port 6667
-          erc-server "irc.freenode.net"
-          erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477")
-          erc-hide-list '("JOIN" "NICK" "PART" "QUIT"))))
+  (setq erc-nick "cmack"
+        erc-autojoin-channels-alist '(("#lisp" "#yui" "#sbcl" "#ruby"))
+        erc-email-userid "charlie"
+        erc-modules '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate)
+        erc-port 6667
+        erc-server "irc.freenode.net"
+        erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477")
+        erc-hide-list '("JOIN" "NICK" "PART" "QUIT")))
 
 (provide 'init-erc)
