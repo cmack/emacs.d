90a1f39119a06d30443cfb73aa13adb09b98db6f
Charlie McMackin <charlie@appa.attlocal.net>
2015-11-02 08:33:42 -0600

    Better the deferment for init-web configs

--- init/init-web.el
+++ init/init-web.el
@@ -3,59 +3,74 @@
 (use-package restclient :ensure t :defer t)
 
 (use-package scss-mode
-  :ensure t :defer t
+  :ensure t
+  :mode ("\\.scss\\'")
   :config
   (setq scss-compile-at-save nil))
 
-(use-package sass-mode :ensure t :defer t)
-(use-package markdown-mode :ensure t :defer t)
+(use-package sass-mode
+  :disabled t
+  :ensure t
+  :mode ("\\.scss\\'"))
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.md\\'"))
 
 (use-package emmet-mode
   :ensure t
-  :commands emmet-mode)
+  :commands emmet-mode
+  :config
+  (add-hook 'web-mode-hook #'emmet-mode)
+  (add-hook 'html-mode-hook #'emmet-mode))
 
 (use-package web-mode
   :ensure t
-  :defer t
+  :commands web-mode
   :mode ("\\.hbs\\'"
          "\\.jsx\\'"
          "/\\([Vv]iews\\|[Hh]tml\\|[Tt]emplates\\)/.*\\.php\\'")
   :config
-  (progn
-    (setq sgml-basic-offset 4)
-    (setq indent-tabs-mode t
-          tab-width 4
-          web-mode-code-indent-offset 2
-          web-mode-css-indent-offset 2
-          web-mode-markup-indent-offset 4)
+  (setq sgml-basic-offset 4)
+  (setq indent-tabs-mode t
+        tab-width 4
+        web-mode-code-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-markup-indent-offset 4)
 
-    (setq web-mode-ac-sources-list
-          '(("php" . (ac-source-yasnippet))
-            ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
-            ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
-    (subword-mode 1)
-    (emmet-mode 1)
+  (setq web-mode-ac-sources-list
+        '(("php" . (ac-source-yasnippet))
+          ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+          ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
 
-    (add-hook 'before-save-hook 'delete-trailing-whitespace)
+  (subword-mode)
+  (emmet-mode)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-    (add-hook 'web-mode-before-auto-complete-hooks
-              '(lambda ()
-                 (let ((web-mode-cur-language (web-mode-language-at-pos)))
-                   (if (string= web-mode-cur-language "php")
-                       (yas-activate-extra-mode 'php-mode)
-                     (yas-deactivate-extra-mode 'php-mode))
-                   (if (string= web-mode-cur-language "css")
-                       (setq emmet-use-css-transform t)
-                     (setq emmet-use-css-transform nil)))))
+  (add-hook 'web-mode-before-auto-complete-hooks
+            '(lambda ()
+               (let ((web-mode-cur-language (web-mode-language-at-pos)))
+                 (if (string= web-mode-cur-language "php")
+                     (yas-activate-extra-mode 'php-mode)
+                   (yas-deactivate-extra-mode 'php-mode))
+                 (if (string= web-mode-cur-language "javascript")
+                     (yas-activate-extra-mode 'js2-mode)
+                   (yas-deactivate-extra-mode 'js2-mode))
+                 (if (string= web-mode-cur-language "css")
+                     (setq emmet-use-css-transform t)
+                   (setq emmet-use-css-transform nil)))))
 
-    (add-hook 'html-mode 'web-mode)))
+  (add-hook 'html-mode-hook 'web-mode))
 
-(use-package sgml-mode
-  :defer t
+(use-package tagedit
+  :ensure t
+  :commands tagedit-mode
   :config
-  (progn
-    (use-package tagedit :ensure t)
-    (tagedit-add-paredit-like-keybindings)
-    (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1)))))
+  (tagedit-add-paredit-like-keybindings)
+
+  (add-hook 'sgml-mode-hook 'tagedit-mode)
+  (add-hook 'html-mode-hook 'tagedit-mode)
+  ;; (add-hook 'web-mode-hook 'tagedit-mode)
+  )
 
 (provide 'init-web)
