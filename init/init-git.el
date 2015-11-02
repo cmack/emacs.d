ecd452c68d2e7b6483f5ec1f6f1feffadf88ebfb
Charlie McMackin <charlie@appa.attlocal.net>
2015-11-02 08:27:15 -0600

    Bind C-x g to MAGIT-STATUS

--- init/init-git.el
+++ init/init-git.el
@@ -1,6 +1,7 @@
 (use-package magit
   :ensure t
-  :defer t
+  :bind
+  (("C-x g" . magit-status))
   :config
   (setq magit-save-some-buffers nil
         magit-last-seen-setup-instructions "1.4.0"))
