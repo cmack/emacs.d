5f04b50878025883796594041cf6c7ff2bf459a0
Charlie McMackin <charlie@appa.attlocal.net>
2015-11-02 08:28:29 -0600

    Change init-python to use USE-PACKAGE

--- init/init-python.el
+++ init/init-python.el
@@ -1,21 +1,27 @@
-(require-package 'jedi)
-;; (require 'ipython)
+(use-package python-mode
+  :config
+  (use-package jedi :disabled t)
+
+  (use-package ipython
+    :disabled t
+    :config
+    (setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+  (use-package elpy
+    :commands elpy-enable
+    :config
+    (setq elpy-rpc-python-command "python3"))
+  (elpy-enable))
 
 
-(elpy-enable)
-
-(setq elpy-rpc-python-command "python3"
-      python-shell-interpreter "ipython3")
-
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args ""
-;;       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;       python-shell-completion-setup-code
-;;       "from IPython.core.completerlib import module_completion"
-;;       python-shell-completion-module-string-code
-;;       "';'.join(module_completion('''%s'''))\n"
-;;       python-shell-completion-string-code
-;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 (provide 'init-python)
