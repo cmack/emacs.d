c9eb35c20044a2e1150dc40d1ad5ce96cd7752b5
Charlie McMackin <charliemac@gmail.com>
2015-11-12 12:03:59 -0600

    Add mac support to tex config

--- init/init-tex.el
+++ init/init-tex.el
@@ -10,10 +10,19 @@
 (use-package tex
   :defer t
   :config
-  (setq TeX-view-program-selection '(((output-dvi style-pstricks)
-                                      "dvips and gv")
-                                     (output-dvi "xdvi")
-                                     (output-pdf "PDF Tools")
-                                     (output-html "xdg-open"))))
+  (unless (assoc "PDF Tools" TeX-view-program-list-builtin)
+    (push '("PDF Tools" TeX-pdf-tools-sync-view) TeX-view-program-list))
+
+  (setq TeX-view-program-selection
+        (cond (*linux-p*
+               '(((output-dvi style-pstricks)
+                  "dvips and gv")
+                 (output-dvi "xdvi")
+                 (output-pdf "PDF Tools")
+                 (output-html "xdg-open")))
+              (*mac-p*
+               '((output-dvi "open")
+                 (output-pdf "PDF Tools")
+                 (output-html "open"))))))
 
 (provide 'init-tex)
