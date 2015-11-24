3fd2202a2e1474fc8ae6425c090bce73ceb7f63b
Charlie McMackin <charliemac@gmail.com>
2015-11-24 12:00:08 -0600

    Clean up php-mode a bit; faster load time
    * remove commented code since switch to ggtags
    * move unnecessary items from :init and :preface to :config to better
      deferring process; speeding up load times.

--- init/init-php.el
+++ init/init-php.el
@@ -3,21 +3,17 @@
   :commands ggtags-mode
   :diminish ggtags-mode)
 
-
 (use-package php-mode
   :ensure t
-  :defer t
   :bind ("C--" . cmack/php-quick-arrow)
-  :init
+  :config
   (use-package php-extras :defer t)
-  (use-package s :ensure t)
 
   (defun cmack/php-quick-arrow ()
     "Inserts -> at point"
     (interactive)
     (insert "->"))
 
-  :preface
   (defun cmack/php-mode-hook ()
     (emmet-mode 1)
     (flycheck-mode 1)
@@ -27,23 +23,12 @@
     (electric-pair-mode)
     (electric-layout-mode))
 
-  :config
   (setq php-executable "/usr/bin/php")
   (setq php-mode-coding-style 'psr2)
   (setq tab-width 4
         fill-column 119
         indent-tabs-mode nil)
-  (add-hook 'php-mode-hook #'cmack/php-mode-hook)
-  ;; (setq projectile-tags-command
-  ;;       (s-join " "
-  ;;               (list "ctags -Re"
-  ;;                     "--languages=-javascript,sql"
-  ;;                     "--exclude=.git"
-  ;;                     "--PHP-kinds=+cf"
-  ;;                     (shell-quote-argument "--regex-PHP='/abstract\s+class\s+([^ ]*)/\\1/c/'")
-  ;;                     (shell-quote-argument "--regex-PHP='/interface\s+([^ ]*)/\\1/c/'")
-  ;;                     (shell-quote-argument "--regex-PHP='/(public\s+|static\s+|abstract\s+|protected\s+|private\s+)+function\s+([^ (]*)/\2/f/'"))
-  ;;               ))
-  )
+
+  (add-hook 'php-mode-hook #'cmack/php-mode-hook))
 
 (provide 'init-php)
