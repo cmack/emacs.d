684f387b0a6ae8993b4a534a065fea6d63c71346
Charlie McMackin <charliemac@gmail.com>
2016-12-07 09:58:08 -0600

    Add sqlup-mode and improve sql-indent
    * ensure sqlup-mode package
    
    * move sql-indent config to top level
    
    * add separate top-level config for sql interactive mode

--- init/init-sql.el
+++ init/init-sql.el
@@ -1,15 +1,27 @@
+(use-package sqlup-mode
+  :ensure t
+  :commands sqlup-mode)
+
+(use-package sql-indent
+  :ensure t
+  :after (sql-mode sql-interactive-mode)
+  :commands (sql-indent-buffer sql-indent-line)
+  :config
+  (setq sql-indent-offset 2))
+
 (use-package sql
   :commands sql-mode
   :config
-  (use-package sql-indent
-    :ensure t)
+  (add-hook 'sql-mode-hook #'sqlup-mode)
+  (setq sql-pop-to-buffer-after-send-region t))
 
-  (add-hook 'sql-interactive-mode-hook
-            (lambda ()
-              (setq sql-prompt-regexp "[_[:alpha:]]*=?[#>] ")
-              (setq sql-prompt-cont-regexp "[_[:alpha:]]*-?[#>] "))))
-
-
-
+(use-package sql-interactive-mode
+  :commands sql-interactive-mode
+  :config
+  (add-hook 'sql-interactive-mode-hook #'sqlup-mode)
+  (toggle-truncate-lines t)
+  ;; Handle databases that have snakecase names
+  (setq sql-prompt-regexp "[_[:alpha:]]*=?[#>] "
+        sql-prompt-cont-regexp "[_[:alpha:]]*-?[#>] "))
 
 (provide 'init-sql)
