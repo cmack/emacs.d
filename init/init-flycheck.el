98a5fd8477803f91991c81f0556919e1aa6350b0
Charlie McMackin <charliemac@gmail.com>
2016-12-24 06:04:30 -0600

    Add eslint for flycheck

--- init/init-flycheck.el
+++ init/init-flycheck.el
@@ -1,8 +1,23 @@
 (use-package flycheck
   :ensure t
-  :defer 5
+  :diminish flycheck-mode
+  :after (projectile)
+  :init (add-hook 'prog-mode-hook #'flycheck-mode)
   :config
-  (global-flycheck-mode 1)
-  :diminish flycheck-mode)
+
+  ;; disable jshint since we prefer eslint checking
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
+
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  (defun cmack/add-projectile-root-to-eslint ()
+    (make-variable-buffer-local 'flycheck-eslint-rules-directories)
+    (when (projectile-project-p)
+      (add-to-list 'flycheck-eslint-rules-directories (projectile-project-root))))
+
+  (add-hook 'global-flycheck-mode-hook #'cmack/add-projectile-root-to-eslint)
+  (add-hook 'flycheck-mode-hook #'cmack/add-projectile-root-to-eslint))
 
 (provide 'init-flycheck)
