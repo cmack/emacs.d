d4493c2e098a176c726ee1a9b72e7f8fbca98235
Charlie McMackin <charliemac@gmail.com>
2017-01-26 05:39:07 -0600

    Rearrange helm config
    * Make top-level all `use-package` forms
    
    * Move helm-descbinds to its config
    
    * Add helm-gtags-auto-update

--- init/init-helm.el
+++ init/init-helm.el
@@ -2,29 +2,15 @@
   :ensure t
   :after (helm helm-projectile))
 
-(use-package helm-config
-  :defer 0.1
-  :config
-  (use-package helm
+(use-package helm-config :defer 1)
+
+(use-package helm
     :ensure t
     :diminish helm-mode
-    :commands helm-mode
-    :config
-    (use-package helm-descbinds :ensure t)
-    (setq helm-candidate-number-limit 100)
-    ;; From https://gist.github.com/antifuchs/9238468
-    (setq helm-idle-delay 0.0    ; update fast sources immediately (doesn't).
-          helm-input-idle-delay 0.01   ; this actually updates things
-                                        ; reeeelatively quickly.
-          helm-yas-display-key-on-candidate t
-          helm-quick-update t
-          helm-M-x-requires-pattern nil
-          helm-ff-skip-boring-files t)
-    (helm-mode 1)
+    :after helm-config
+    ;; :commands helm-mode
     :bind  (("C-c h" . helm-mini)
             ("C-h a" . helm-apropos)
-            ("C-h b" . helm-descbinds)
-            ("C-h w" . helm-descbinds)
             ("C-x b" . helm-buffers-list)
             ("C-x C-b" . helm-buffers-list)
             ("C-x C-f" . helm-find-files)
@@ -34,13 +20,34 @@
             ;; ("C-x c Y" . helm-yas-create-snippet-on-region)
             ("C-x c SPC" . helm-all-mark-rings)
             ("M-y" . helm-show-kill-ring)
-            ("M-x" . helm-M-x))))
+            ("M-x" . helm-M-x))
+
+    :config
+    (setq helm-candidate-number-limit 100)
+    ;; From https://gist.github.com/antifuchs/9238468
+    (setq helm-idle-delay 0.0    ; update fast sources immediately (doesn't).
+          helm-input-idle-delay 0.01   ; this actually updates things
+                                        ; reeeelatively quickly.
+          helm-yas-display-key-on-candidate t
+          helm-quick-update t
+          helm-M-x-requires-pattern nil
+          helm-ff-skip-boring-files t)
+    (helm-mode 1))
+
+(use-package helm-descbinds
+  :ensure t
+  :after helm
+  :bind (("C-h b" . helm-descbinds)
+         ("C-h w" . helm-descbinds)))
 
 (use-package helm-gtags
   :ensure t
   :commands helm-gtags-mode
   :bind (("M-." . helm-gtags-dwim)
          ("M-," . helm-gtags-pop-stack)
-         ("M-r" . helm-gtags-find-rtag)))
+         ("M-r" . helm-gtags-find-rtag))
+
+  :config
+  (custom-set-variables '(helm-gtags-auto-update t)))
 
 (provide 'init-helm)
