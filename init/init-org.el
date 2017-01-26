6424daaef03e3f63e5838fe5ad8859cda68e13f5
Charlie McMackin <charliemac@gmail.com>
2017-01-26 05:23:27 -0600

    Add key bindings and misc customizations for org-mode
    * add indentation
    
    * allow alphabetical lists
    
    * pull initialization of ox-md to toplevel form and don't "ensure" since it's
      part of org, just not loaded by default.

--- init/init-org.el
+++ init/init-org.el
@@ -3,8 +3,18 @@
   :init
   (setq org-agenda-files (list "~/Dropbox/org/work.org"
                                "~/Dropbox/org/home.org"))
-  :config
-  (use-package ox-md :ensure t))
+  :bind
+  ("C-c a" . org-agenda)
+  ("C-c r" . org-capture)
 
+  :config
+  (setq org-pretty-entities t
+        org-return-follows-link t
+        org-startup-indented t
+        org-src-preserve-indentation t
+        org-list-allow-alphabetical t))
+
+(use-package ox-md
+  :after org)
 
 (provide 'init-org)
