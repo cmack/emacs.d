341223c7d4f261bcccad4e802a75d12f85beb763
Charlie McMackin <charliemac@gmail.com>
2017-01-28 06:49:19 -0600

    Fix init-helm whitespace

--- init/init-helm.el
+++ init/init-helm.el
@@ -5,34 +5,34 @@
 (use-package helm-config :defer 1)
 
 (use-package helm
-    :ensure t
-    :diminish helm-mode
-    :after helm-config
-    ;; :commands helm-mode
-    :bind  (("C-c h" . helm-mini)
-            ("C-h a" . helm-apropos)
-            ("C-x b" . helm-buffers-list)
-            ("C-x C-b" . helm-buffers-list)
-            ("C-x C-f" . helm-find-files)
-            ("C-x c o" . helm-occur)
-            ;; ("C-x c s" . helm-swoop)
-            ;; ("C-x c y" . helm-yas-complete)
-            ;; ("C-x c Y" . helm-yas-create-snippet-on-region)
-            ("C-x c SPC" . helm-all-mark-rings)
-            ("M-y" . helm-show-kill-ring)
-            ("M-x" . helm-M-x))
+  :ensure t
+  :diminish helm-mode
+  :after helm-config
+  ;; :commands helm-mode
+  :bind  (("C-c h" . helm-mini)
+          ("C-h a" . helm-apropos)
+          ("C-x b" . helm-buffers-list)
+          ("C-x C-b" . helm-buffers-list)
+          ("C-x C-f" . helm-find-files)
+          ("C-x c o" . helm-occur)
+          ;; ("C-x c s" . helm-swoop)
+          ;; ("C-x c y" . helm-yas-complete)
+          ;; ("C-x c Y" . helm-yas-create-snippet-on-region)
+          ("C-x c SPC" . helm-all-mark-rings)
+          ("M-y" . helm-show-kill-ring)
+          ("M-x" . helm-M-x))
 
-    :config
-    (setq helm-candidate-number-limit 100)
-    ;; From https://gist.github.com/antifuchs/9238468
-    (setq helm-idle-delay 0.0    ; update fast sources immediately (doesn't).
-          helm-input-idle-delay 0.01   ; this actually updates things
+  :config
+  (setq helm-candidate-number-limit 100)
+  ;; From https://gist.github.com/antifuchs/9238468
+  (setq helm-idle-delay 0.0    ; update fast sources immediately (doesn't).
+        helm-input-idle-delay 0.01   ; this actually updates things
                                         ; reeeelatively quickly.
-          helm-yas-display-key-on-candidate t
-          helm-quick-update t
-          helm-M-x-requires-pattern nil
-          helm-ff-skip-boring-files t)
-    (helm-mode 1))
+        helm-yas-display-key-on-candidate t
+        helm-quick-update t
+        helm-M-x-requires-pattern nil
+        helm-ff-skip-boring-files t)
+  (helm-mode 1))
 
 (use-package helm-descbinds
   :ensure t
