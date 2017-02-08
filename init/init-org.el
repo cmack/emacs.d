0f43f9d9b2c9176b92f7edf8df60b371b3236247
Charlie McMackin <charliemac@gmail.com>
2017-02-08 13:08:18 -0600

    Enable visual-line-mode in org-mode

--- init/init-org.el
+++ init/init-org.el
@@ -8,11 +8,16 @@
   ("C-c r" . org-capture)
 
   :config
+  (defun cmack/org-mode-hook ()
+    (visual-line-mode t))
+
   (setq org-pretty-entities t
         org-return-follows-link t
         org-startup-indented t
         org-src-preserve-indentation t
-        org-list-allow-alphabetical t))
+        org-list-allow-alphabetical t)
+
+  (add-hook 'org-mode-hook #'cmack/org-mode-hook))
 
 (use-package ox-md
   :after org)
