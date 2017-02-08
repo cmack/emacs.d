572157554ac4e09bac8cc4cf07d8cfab059e5e26
Charlie McMackin <charliemac@gmail.com>
2017-02-08 11:05:14 -0600

    Add more keybindings for smartparens
    * Ensure C-d gets mapped in php-mode to `sp-delete-char`

--- init/init-php.el
+++ init/init-php.el
@@ -8,10 +8,16 @@
 (use-package smartparens
   :ensure smartparens
   :commands (smartparens turn-on-smartparens-strict-mode)
-  :bind (("C-M-f" . sp-forward-sexp)
+  :bind (("C-d" . sp-delete-char)
+         ("C-M-f" . sp-forward-sexp)
          ("C-M-b" . sp-backward-sexp)
          ("C-M-k" . sp-kill-sexp)
+         ("C-M-e" . sp-up-sexp)
+         ("C-M-n" . sp-next-sexp)
+         ("C-M-p" . sp-previous-sexp)
+         ("C-M-s" . sp-unwrap-sexp)
          ("C-M-t" . sp-transpose-sexp)
+         ("C-M-<backspace>" . sp-backward-unwrap-sexp)
          ("C-M-w" . sp-copy-sexp)
          ("C-<right>" . sp-slurp-hybrid-sexp)
          ("C-<left>" . sp-forward-barf-sexp)
@@ -44,7 +50,9 @@
 
 (use-package php-mode
   :ensure t
-  :bind (("C--" . cmack/php-quick-arrow))
+  :bind (("C--" . cmack/php-quick-arrow)
+         :map php-mode-map
+         ("C-d" . sp-delete-char))
   :config
 
   (defun cmack/php-quick-arrow (arg)
