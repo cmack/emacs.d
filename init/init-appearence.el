0b832342d34d10a55fb765f18eaca84ac11f39c1
Charlie McMackin <charliemac@gmail.com>
2017-02-16 09:29:05 -0600

    Refactor appearance slightly
    * use `use-package`'s `:if` predication
    
    * refactor successive `when`s to `cond` for setting font based on platform

--- init/init-appearence.el
+++ init/init-appearence.el
@@ -1,24 +1,18 @@
-;; TODO see if there's a better way
-(when *linux-p*
-  ;; (add-to-list 'default-frame-alist '(font . "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" ))
-  (add-to-list 'default-frame-alist '(font . "-*-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" )))
+(add-to-list 'default-frame-alist
+             (cond (*linux-p*
+                    '(font . "-*-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" ))
+                   (*mac-p*
+                    '(font . "-*-Source Code Pro-regular-r-normal-*-14-*-*-*-m-0-iso10646-1" ))
+                   (*windows-p*
+                    '(font . "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" ))))
 
-(when *mac-p*
-  ;; (add-to-list 'default-frame-alist '(font . "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" ))
-  (add-to-list 'default-frame-alist '(font . "-*-Source Code Pro-regular-r-normal-*-14-*-*-*-m-0-iso10646-1" )))
-
-(when *windows-p*
-  (add-to-list 'default-frame-alist '(font . "-unknown-Inconsolata-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1" )))
-
-
-
-(when (< emacs-major-version 24)
-  (use-package color-theme))
+(use-package color-theme
+  :if (< emacs-major-version 24))
 
 (use-package planet-theme
   :ensure t
+  :if (and window-system (>= emacs-major-version 24))
   :config
-  (when (and window-system (>= emacs-major-version 24))
-    (load-theme 'planet t)))
+  (load-theme 'planet t))
 
 (provide 'init-appearence)
