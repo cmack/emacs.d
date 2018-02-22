8a0a1f220fbb68bb2037a5e78eeb8191e36efc77
Charlie McMackin <charliemac@gmail.com>
2018-02-21 23:33:37 -0600

    Update python configuration
    Use :after of use-package

--- init/init-python.el
+++ init/init-python.el
@@ -1,27 +1,35 @@
-(use-package python-mode
+(use-package elpy
+  :commands python-mode
   :config
-  (use-package jedi :disabled t)
-
-  (use-package ipython
-    :disabled t
-    :config
-    (setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args ""
-      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-      python-shell-completion-setup-code
-      "from IPython.core.completerlib import module_completion"
-      python-shell-completion-module-string-code
-      "';'.join(module_completion('''%s'''))\n"
-      python-shell-completion-string-code
-      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
-
-  (use-package elpy
-    :commands elpy-enable
-    :config
-    (setq elpy-rpc-python-command "python3"))
+  (setq elpy-rpc-python-command "python3")
   (elpy-enable))
 
+(use-package jedi
+  :disabled t
+  :after python-mode)
 
+(use-package ipython
+  :disabled t
+  :after python-mode
+  :config
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args ""
+        python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+        python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+        python-shell-completion-setup-code
+        "from IPython.core.completerlib import module_completion"
+        python-shell-completion-module-string-code
+        "';'.join(module_completion('''%s'''))\n"
+        python-shell-completion-string-code
+        "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+(use-package python-mode
+  :disabled t)
+
+(use-package python
+  :commands python-mode
+  :config
+  (setq python-shell-interpreter "python3")
+  (elpy-enable))
 
 (provide 'init-python)
