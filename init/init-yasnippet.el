2a49632f09f1f66c27e8692dfa5f2f959d875996
Charlie McMackin <charliemac@gmail.com>
2018-02-21 23:05:19 -0600

    Update yasnippet config
    Use more modern features of use-package, etc.

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -1,17 +1,10 @@
 (use-package yasnippet
   :ensure t
   :commands yas-global-mode
-  :init
-  (add-hook 'after-init-hook #'yas-global-mode)
-  :config
-  (setq yas-snippet-dirs (list (expand-file-name
-                                (concat user-emacs-directory "snippets"))))
-  (add-to-list 'yas-snippet-dirs
-               (concat (file-name-directory (locate-library "yasnippet"))
-                       "snippets"))
-
-  (add-hook 'html-mode-hook (lambda ()
-                              (setq ac-sources '(ac-source-semantic ac-source-yasnippet)))))
+  :hook ((html-mode . (lambda ()
+                        (setq ac-sources
+                              '(ac-source-semantic ac-source-yasnippet))))
+         (after-init . yas-global-mode)))
 
 (use-package yasnippet-snippets
   :ensure t
