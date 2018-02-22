299661be81935f66f9d89d6faa56f71c06442477
Charlie McMackin <charliemac@gmail.com>
2018-02-21 23:29:41 -0600

    Update company config
    * add config so that C-n and C-p work

--- init/init-company.el
+++ init/init-company.el
@@ -1,7 +1,10 @@
 (use-package company
   :ensure t
   :commands company-mode
-  :init
-  (add-hook 'prog-mode-hook 'company-mode))
+  :hook ((prog-mode . company-mode))
+  :bind (("C-c C-y" . company-yasnippet)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous)))
 
 (provide 'init-company)
