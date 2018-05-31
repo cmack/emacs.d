7d3b33ecca591fbf6a56eb8a50199a6ffb82b160
Charlie McMackin <charliemac@gmail.com>
2018-05-31 17:12:20 -0500

    Add keybinding for `magit-log-buffer-file`
    Great for getting the log history of current buffer; even greater for getting
    log of a selected region

--- init/init-git.el
+++ init/init-git.el
@@ -1,7 +1,8 @@
 (use-package magit
   :ensure t
   :bind
-  (("C-x g" . magit-status))
+  (("C-x g" . magit-status)
+   ("C-c g l" . magit-log-buffer-file))
   :config
   (setq magit-save-some-buffers nil
         magit-last-seen-setup-instructions "1.4.0"))
