1d3e131d7ae04a1d73d34b2821c468505f0e5e87
Charlie McMackin <charliemac@gmail.com>
2018-05-31 17:08:43 -0500

    Update sql config for new use-package features
    * Use `:hook` to replace the `add-hook` calls in other definitions
    * Use `:any`  in `sql-indent` config to ensure it's in either modes

--- init/init-sql.el
+++ init/init-sql.el
@@ -1,24 +1,22 @@
 (use-package sqlup-mode
   :ensure t
-  :commands sqlup-mode)
+  :commands sqlup-mode
+  :hook (sql-mode sql-interactive-mode))
 
 (use-package sql-indent
   :ensure t
-  :after (sql-mode sql-interactive-mode)
-  :commands (sql-indent-buffer sql-indent-line)
+  :after (:any sql sql-interactive-mode)
   :config
   (setq sql-indent-offset 2))
 
 (use-package sql
   :commands sql-mode
   :config
-  (add-hook 'sql-mode-hook #'sqlup-mode)
   (setq sql-pop-to-buffer-after-send-region t))
 
 (use-package sql-interactive-mode
   :commands sql-interactive-mode
   :config
-  (add-hook 'sql-interactive-mode-hook #'sqlup-mode)
   (toggle-truncate-lines t)
   ;; Handle databases that have snakecase names
   (setq sql-prompt-regexp "[_[:alpha:]]*=?[#>] "
