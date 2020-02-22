66fd5b90ee7144bfb6f57b87dea299fafea8cf9e
Charlie McMackin <charliemac@gmail.com>
2020-02-22 00:57:43 -0600

    Enable magit-file-mode-map and configure magit-file-dispatch

--- init/init-git.el
+++ init/init-git.el
@@ -2,7 +2,10 @@
   :ensure t
   :bind
   (("C-x g" . magit-status)
-   ("C-c g l" . magit-log-buffer-file))
+   ;; ("C-c g l" . magit-log-buffer-file)
+   :map magit-file-mode-map
+   ("C-c g" . magit-file-dispatch))
+
   :config
   (setq magit-save-some-buffers nil
         magit-last-seen-setup-instructions "1.4.0"))
