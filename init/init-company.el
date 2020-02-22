f057830a38e9d447ed3ac6d90f0d137eae8b7a1b
Charlie McMackin <charliemac@gmail.com>
2020-02-22 00:53:08 -0600

    Update company config
    Space and Shift Space go forward and backward in candidate selections, similar
    to Japanese input I'm used to.

--- init/init-company.el
+++ init/init-company.el
@@ -4,10 +4,15 @@
   :hook ((prog-mode . company-mode))
   :bind (("C-c C-y" . company-yasnippet)
          :map company-active-map
+         ("SPC" . company-select-next)
+         ("S-SPC" . company-select-previous)
          ("C-n" . company-select-next)
          ("C-p" . company-select-previous))
   :config
-  (setq company-auto-complete t))
+  (setq company-auto-complete t
+        company-idle-delay 0.3
+        company-show-numbers t
+        company-tooltip-align-annotations t))
 
 (use-package company-lsp
   :ensure t
