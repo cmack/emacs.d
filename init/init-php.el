be1cc616c4bf106ae2d148af654d95e076b4e9b4
Charlie McMackin <charliemac@gmail.com>
2020-08-02 10:38:06 -0500

    Update smartparens config
    use out-of-box paredit mapping

--- init/init-php.el
+++ init/init-php.el
@@ -10,25 +10,10 @@
 (use-package smartparens
   :ensure smartparens
   :commands (smartparens turn-on-smartparens-strict-mode)
-  :bind (("C-d" . sp-delete-char)
-         ("C-M-f" . sp-forward-sexp)
-         ("C-M-b" . sp-backward-sexp)
-         ("C-M-k" . sp-kill-sexp)
-         ("C-M-e" . sp-up-sexp)
-         ("C-M-n" . sp-next-sexp)
-         ("C-M-p" . sp-previous-sexp)
-         ("C-M-s" . sp-unwrap-sexp)
-         ("C-M-t" . sp-transpose-sexp)
-         ("C-M-<backspace>" . sp-backward-unwrap-sexp)
-         ("C-M-w" . sp-copy-sexp)
-         ("C-<right>" . sp-slurp-hybrid-sexp)
-         ("C-<left>" . sp-forward-barf-sexp)
-         ("C-M-<left>" . sp-backward-slurp-sexp)
-         ("C-M-<right>" . sp-backward-barf-sexp)
-         ("M-F" . sp-forward-symbol)
-         ("M-B" . sp-backward-symbol))
-  :config
-  (use-package smartparens-config))
+  :config (sp-use-paredit-bindings))
+
+(use-package smartparens-config
+  :after smartparens)
 
 (use-package php-extras
   :ensure t
