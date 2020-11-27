3af683b83b6fa28e83f762c9abf835629c27153c
Charlie McMackin <charliemac@gmail.com>
2020-11-27 06:51:19 -0600

    Clean up company config
    * Remove extaneous :config section
    * Disable auto-commit for now -- I feel like this is causing annoyance
    * Enable selection wrap around

--- init/init-company.el
+++ init/init-company.el
@@ -1,19 +1,20 @@
 (use-package company
   :ensure t
   :commands company-mode
-  :config
-  (setq company-auto-complete t)
   :hook ((prog-mode . company-mode))
   :bind (("C-c C-y" . company-yasnippet)
          :map company-active-map
          ("SPC" . company-select-next)
          ("S-SPC" . company-select-previous)
          ("C-n" . company-select-next)
-         ("C-p" . company-select-previous))
+         ("C-p" . company-select-previous)
+         ;; ("Enter" . company-complete-selection)
+         )
   :config
-  (setq company-auto-complete t
+  (setq company-auto-commit nil
         company-idle-delay 0.3
         company-show-numbers t
-        company-tooltip-align-annotations t))
+        company-tooltip-align-annotations t
+        company-selection-wrap-around t))
 
 (provide 'init-company)
