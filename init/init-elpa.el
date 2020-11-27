ea2aac46ab413f25868a0d9128ce288be047bf8e
Charlie McMackin <charliemac@gmail.com>
2020-11-27 10:00:46 -0600

    Add initial support for csv-mode
    CSV-ALIGN-MODE and CSV-HEADER-LINE did not work except through the mode hook.

--- init/init-elpa.el
+++ init/init-elpa.el
@@ -49,6 +49,19 @@
   :ensure t
   :commands rainbow-delimiters-mode)
 
+(defun cmack/csv-mode-hook ()
+  "My custom csv mode hook.
+These don't work in :config of (use-package csv-mode )."
+  (csv-align-mode 1)
+  (csv-header-line))
+
+(use-package csv-mode
+  :ensure t
+  :mode (".tsv" ".csv" ".tabular" ".vcf")
+  :config
+  (toggle-truncate-lines 1)
+  :hook (csv-mode . cmack/csv-mode-hook))
+
 ;; (require-package 'ace-jump-mode)
 ;; (require-package 'fill-column-indicator)
 ;; (require-package 'expand-region)
