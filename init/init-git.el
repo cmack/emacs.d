8b47ef76dabda8877c43e758ab2ebca16b18a66d
Charlie McMackin <charliemac@gmail.com>
2020-11-27 06:32:06 -0600

    Update magit config for latest upstream version
    * Fix error in hook. It can't be loaded after-init since magit isn't available
      then. Also `global-magit-file-mode` is the default, so use it.
    
    * Fix `magit-file-mode-map` bindings. They are no longer necessary with
      `global-magit-file-mode`

--- init/init-git.el
+++ init/init-git.el
@@ -2,14 +2,10 @@
   :ensure t
   :bind
   (("C-x g" . magit-status)
-   ;; ("C-c g l" . magit-log-buffer-file)
-   :map magit-file-mode-map
    ("C-c g" . magit-file-dispatch))
 
   :config
-  (setq-default magit-diff-refine-hunk t)
-  (setq magit-last-seen-setup-instructions "1.4.0")
-  :hook (after-init . global-magit-file-mode))
+  (setq-default magit-diff-refine-hunk t))
 
 
 (provide 'init-git)
