156fa4928be8a8d8546c417cd509ef33e00aecbe
Charlie McMackin <charliemac@gmail.com>
2021-01-15 01:05:52 -0600

    Add header to init.el

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-;; -*- coding: utf-8 -*-
+;;; init.el --- My config     -*- coding: utf-8; lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
