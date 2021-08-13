dc166149bf18f6d9c7806ae3e3196475c6fb84ec
Charlie McMackin <charliemac@gmail.com>
2021-08-13 13:08:21 -0500

    Update org-mode config to use :hook

--- init/init-org.el
+++ init/init-org.el
@@ -3,28 +3,28 @@
 ;;; Code:
 
 (use-package org
-  :defer t
+  :commands (org-mode org-agenda org-capture)
   :init
   (setq org-agenda-files (list "~/Dropbox/org/work.org"
                                "~/Dropbox/org/home.org"))
+  (defun cmack/org-mode-hook ()
+    (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t)
+    (push '(company-capf :with company-yasnippet) company-backends))
+
   :bind
   ("C-c a" . org-agenda)
   ("C-c r" . org-capture)
 
   :config
-  (defun cmack/org-mode-hook ()
-    (visual-line-mode t)
-    (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t)
-    (company-mode t)
-    (push '(company-capf :with company-yasnippet) company-backends))
-
   (setq org-pretty-entities t
         org-return-follows-link t
         org-startup-indented t
         org-src-preserve-indentation t
         org-list-allow-alphabetical t)
-
-  (add-hook 'org-mode-hook #'cmack/org-mode-hook))
+  :hook
+  ((org-mode . visual-line-mode)
+   (org-mode . company-mode)
+   (org-mode . cmack/org-mode-hook)))
 
 (use-package ox-md
   :after org)
