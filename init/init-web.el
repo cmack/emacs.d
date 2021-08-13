4fc481ccf208b350a44e4dc7e736c1a45c113377
Charlie McMackin <charliemac@gmail.com>
2021-08-13 12:49:33 -0500

    Fix warning message during init for web-mode config

--- init/init-web.el
+++ init/init-web.el
@@ -57,7 +57,7 @@
 
   :init
   (add-hook 'web-mode-before-auto-complete-hooks
-            '(lambda ()
+            #'(lambda ()
                (let ((web-mode-cur-language (web-mode-language-at-pos)))
                  (if (string= web-mode-cur-language "php")
                      (yas-activate-extra-mode 'php-mode)
