ea68e2102c113e2935327ffe7d07ae2206104d09
Charlie McMackin <charlie.mac@gmail.com>
2022-11-21 23:07:25 -0600

    Update erc config; default to libera.chat
    Use :custom syntax of USE-PACKAGE

--- init/init-erc.el
+++ init/init-erc.el
@@ -1,13 +1,14 @@
 (use-package erc
-  :commands erc
-  :config
-  (setq erc-nick "cmack"
-        erc-autojoin-channels-alist '(("#lisp" "#yui" "#sbcl" "#ruby"))
-        erc-email-userid "charlie"
-        erc-modules '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate)
-        erc-port 6667
-        erc-server "irc.freenode.net"
-        erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477")
-        erc-hide-list '("JOIN" "NICK" "PART" "QUIT")))
+  :commands (erc-tls erc)
+  :custom
+  (erc-nick "cmack")
+  (erc-autojoin-channels-alist '(("libera.chat"
+                                  "#lisp" "#commonlisp" "#clim" "#sbcl" "#lispgames")))
+  (erc-email-userid "charlie")
+  (erc-modules '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track truncate))
+  (erc-port 6697)
+  (erc-server "irc.libera.chat")
+  (erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "333" "353" "324" "329" "332" "477"))
+  (erc-hide-list '("JOIN" "NICK" "PART" "QUIT")))
 
 (provide 'init-erc)
