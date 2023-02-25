3d3961944c633e26cba9bd5749bec60dc92113ce
Charlie McMackin <charlie.mac@gmail.com>
2023-02-25 08:26:32 -0600

    Add corfu config, make default, and disable company

--- init/init-company.el
+++ init/init-company.el
@@ -1,5 +1,7 @@
 (use-package company
   :ensure t
+  :disabled t
+  :commands company-mode
   :hook ((prog-mode . company-mode))
   :bind (("C-c C-y" . company-yasnippet)
          :map company-active-map
@@ -20,5 +22,17 @@
   :after (lsp-mode company)
   :config
   (cl-pushnew 'company-lsp company-backends))
+
+(use-package corfu
+  :ensure t
+  :hook ((prog-mode . corfu-mode))
+  :bind (:map corfu-map
+              ("SPC" . corfu-next)
+              ("S-SPC" . corfu-previous))
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-delay 0.2)
+  (corfu-quit-at-boundary nil))
 
 (provide 'init-company)
